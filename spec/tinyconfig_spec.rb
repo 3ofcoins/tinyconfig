5e91505ec94438381c96dd4c50ac9f40d0ea0ee4
Marta Paciorkowska <marta@3ofcoins.net>
2013-08-19 15:10:20 +0200

    Unifies quotation mark usage.

--- spec/tinyconfig_spec.rb
+++ spec/tinyconfig_spec.rb
@@ -8,18 +8,18 @@ end
 describe TinyConfig do
   let(:cfg) { BasicIdea.new }
 
-  describe '.option' do
+  describe ".option" do
     it "defaults options' values to nil" do
       expect { cfg.opt.nil? }
     end
 
-    it 'sets default to a provided value' do
+    it "sets default to a provided value" do
       expect { cfg.defopt == 23 }
     end
   end
 
-  describe '#configure' do
-    it 'configures in a block' do
+  describe "#configure" do
+    it "configures in a block" do
       cfg.configure do
         opt 23
       end
@@ -28,29 +28,29 @@ describe TinyConfig do
     end
   end
 
-  describe '#load' do
-    it 'loads configuration from file relative to the caller' do
-      cfg.load('fixtures/basic.rb')
+  describe "#load" do
+    it "loads configuration from file relative to the caller" do
+      cfg.load("fixtures/basic.rb")
       expect { cfg.opt == 23 }
     end
 
     it "can be nested" do
-      cfg.load('fixtures/basic_nested.rb')
+      cfg.load("fixtures/basic_nested.rb")
       expect { cfg.opt == 23 }
     end
 
     it "can load files by a glob expression" do
-      cfg.load('fixtures/glob*.rb')
+      cfg.load("fixtures/glob*.rb")
       expect { cfg.opt == 17 }
     end
   end
 
-  describe '(configuration block)' do
-    it 'raises an exception when somebody tries to set undefined value' do
+  describe "(configuration block)" do
+    it "raises an exception when somebody tries to set undefined value" do
       expect { rescuing { cfg.configure { nopt -1 } }.is_a? NoMethodError }
     end
 
-    it 'overrides default values' do
+    it "overrides default values" do
       cfg.configure { defopt 17 }
       expect { cfg.defopt == 17 }
     end
