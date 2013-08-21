c4a6e38b8b9eca741050aa973ebce7c39cae89a9
Marta Paciorkowska <marta@3ofcoins.net>
2013-08-21 21:42:02 +0200

    Refactors new method names for improving readability.

--- lib/tinyconfig.rb
+++ lib/tinyconfig.rb
@@ -56,25 +56,20 @@ class TinyConfig < BasicObject
     # calling file (directory that contains the ruby source file that
     # has called the `TinyConfig#load` method) rather than whatever is
     # the process' `Dir.getwd`.
-    #::Kernel::puts "glob passed as argument: #{glob.inspect}"
+
     glob = ::File.join(::File.dirname(::Kernel.caller.first), glob)
-    #::Kernel::puts "glob after parsing: #{glob.inspect}"
-    ::Kernel::p self
+
     load_helper(glob)
-    
   end
 
-  def load_in_bulk
-    directory_name = ::File.join(::File.dirname(::Kernel.caller[0]), ::File.basename(::Kernel.caller[0], ".*"))
-    #::Kernel::p ::File.basename(::Kernel.caller.first) # => "directory.rb:in `block in load'"
-    #::Kernel::p directory_name # => "/home/.../tinyconfig/spec/fixtures/directory"
-    #::Kernel::p ::File.join(directory_name, "*.rb") # => "/home/.../tinyconfig/spec/fixtures/directory/*.rb"
-    #load(::File.join(directory_name, "*.rb"))
+  def load_in_bulk #Split to three lines for readability.
+    directory_name = ::File.join(::File.dirname(::Kernel.caller.first), ::File.basename(::Kernel.caller.first, ".*"))
     
-    globa = ::File.join(directory_name, "*.rb")
-    
-    load_helper(globa)
+    bulk_file_source = ::File.join(directory_name, "*.rb")
+
+    load_helper(bulk_file_source)
   end
+
   #
   # Compat methods
   # --------------
@@ -88,10 +83,9 @@ class TinyConfig < BasicObject
 
   private
 
-  def load_helper(glob)
-    ::Dir.glob(glob).sort.each do |path|
+  def load_helper(source)
+    ::Dir.glob(source).sort.each do |path|
     self.instance_eval(::File.read(path), path, 0)
-    ::Kernel::p "Loading helper."
     end
   end
   
